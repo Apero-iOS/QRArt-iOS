15cf2fe7e7700012fe1878b822a9827b64ee6bb9
khactao1994 <taodk@apero.vn>
2023-06-26 14:04:09 +0700

    [Init] New project

---
+++ QRArtGenerator/QRArtGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRArtGeneratorApp.swift
+//  QRArtGenerator
+//
+//  Created by khac tao on 26/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct QRArtGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
