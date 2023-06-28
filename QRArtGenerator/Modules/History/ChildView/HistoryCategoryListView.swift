d802b2190a04e7400920fa09f7b37dc3abf18d3e
QuangLHApero <100119435+QuangLHApero@users.noreply.github.com>
2023-06-28 19:11:06 +0700

    [IQA-4] Create UI history (#8)

---
+++ QRArtGenerator/Modules/History/ChildView/HistoryCategoryListView.swift
@@ -0,0 +1,20 @@
+//
+//  HistoryCategoryListView.swift
+//  QRArtGenerator
+//
+//  Created by Quang Ly Hoang on 28/06/2023.
+//
+
+import SwiftUI
+
+struct HistoryCategoryListView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct HistoryCategoryListView_Previews: PreviewProvider {
+    static var previews: some View {
+        HistoryCategoryListView()
+    }
+}
---
+++ QRArtGenerator/Modules/History/ChildView/HistoryListView.swift
@@ -0,0 +1,20 @@
+//
+//  HistoryListView.swift
+//  QRArtGenerator
+//
+//  Created by Quang Ly Hoang on 28/06/2023.
+//
+
+import SwiftUI
+
+struct HistoryListView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct HistoryListView_Previews: PreviewProvider {
+    static var previews: some View {
+        HistoryListView()
+    }
+}
