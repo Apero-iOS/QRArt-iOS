a30dd90038d018294416a05b7dca47da13025069
TuanLT <tuanlt@apero.vn>
2023-07-03 14:21:55 +0700

    update code

--- QRArtGenerator/Modules/Loading/LoadingView.swift
+++ QRArtGenerator/Modules/Loading/LoadingView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct LoadingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            LottieView(lottieFile: R.file.loadingJson.name)
+        }
+        .background(R.color.color_1B232E.color)
     }
 }
 
--- QRArtGenerator/Modules/Result/ResultView.swift
+++ QRArtGenerator/Modules/Result/ResultView.swift
@@ -9,8 +9,29 @@ import SwiftUI
 
 struct ResultView: View {
     var body: some View {
-        VStack {
-            NavibarView()
+        VStack(spacing: 20) {
+            naviView
+            Image(R.image.img_result)
+                .resizable()
+                .padding(EdgeInsets(top: 0, leading: 20, bottom: 20, trailing: 20))
+                .frame(width: WIDTH_SCREEN, height: WIDTH_SCREEN, alignment: .center)
+            VStack {
+                HStack(spacing: 8) {
+                    ResultButtonView(typeButton: .download)
+                    ResultButtonView(typeButton: .download4k)
+                }
+                HStack {
+                    ResultButtonView(typeButton: .regenerate)
+                    ResultButtonView(typeButton: .share)
+                }
+            }
+            Spacer()
+        }
+    }
+    
+    @ViewBuilder var naviView: some View {
+        NavibarView(title: Rlocalizable.create_qr_title(), isImageTitle: true) {
+            // TODO
         }
     }
 }
