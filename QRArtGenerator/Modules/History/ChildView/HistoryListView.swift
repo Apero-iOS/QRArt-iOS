6881f894a7d8a6d9501732105bf08c644952c767
TuanLT <tuanlt@apero.vn>
2023-07-05 09:32:21 +0700

    fix comment

--- QRArtGenerator/Modules/History/ChildView/HistoryListView.swift
+++ QRArtGenerator/Modules/History/ChildView/HistoryListView.swift
@@ -17,16 +17,12 @@ struct HistoryListView: View {
             Section(content: {
                 ForEach(items, id: \.id) { item in
                     let vm = ResultViewModel(item: item as! QRDetailItem, image: Image(uiImage: item.qrImage), source: .history)
-//                    NavigationLink {
-//                        ResultView(viewModel: vm)
-//                    } label: {
-                        HistoryCell(item: item, canDelete: isInHistory, onDelete: {
-                            onDelete?(item)
-                        })
-                        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 12, trailing: 0))
-                        .hideSeparatorLine()
-                        .background(NavigationLink("", destination: ResultView(viewModel: vm)).opacity(0))
-//                    }
+                    HistoryCell(item: item, canDelete: isInHistory, onDelete: {
+                        onDelete?(item)
+                    })
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 12, trailing: 0))
+                    .hideSeparatorLine()
+                    .background(NavigationLink("", destination: ResultView(viewModel: vm)).opacity(0))
                 }
             }, footer: {
                 Color.clear
