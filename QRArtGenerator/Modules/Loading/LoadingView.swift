63747ff46e2c36e517fe2acbf5bed388f66dbd5f
Đinh Văn Trình <trinhdv@apero.vn>
2023-07-17 15:10:55 +0700

    fix loading screen

--- QRArtGenerator/Modules/Loading/LoadingView.swift
+++ QRArtGenerator/Modules/Loading/LoadingView.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct LoadingView: View {
+    
+    @State var scale: CGFloat = 0.75
+    @State var textStr: String = "Generating..."
+    @State var moveDown: Bool = false
+    
     var body: some View {
-        VStack {
+        ZStack {
             LottieView(lottieFile: R.file.loadingJson.name)
+            HStack(spacing: 2) {
+                let textArr = textStr.map { String($0) }
+                ForEach(0..<textArr.count, id: \.self) { index in
+                    Text(textArr[index])
+                        .font(R.font.urbanistSemiBold.font(size: 16))
+                        .foregroundColor(Color(red: 0.98, green: 0.76, blue: 0.16))
+                        .scaleEffect(scale)
+//                        .offset(x: 0, y: self.moveDown ? 3 : 0) 
+                        .animation(
+                            .easeInOut(duration: 0.6)
+                            .repeatForever()
+                            .delay(Double(index)*0.1),
+                            value: UUID()
+                        )
+                        .onAppear {
+                            withAnimation {
+                                scale = 1
+//                                moveDown.toggle()
+                            }
+                        }
+                }
+            }
+            .padding(.top, 100)
+            
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(R.color.color_1B232E.color)
+        
     }
 }
 
