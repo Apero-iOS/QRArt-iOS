a0d7b70e698b7b2597c392382b0cde31e30d7e80
khactao1994 <taodk@apero.vn>
2023-08-14 10:03:07 +0700

    [Update] update id ad and remote key

--- QRArtGenerator/Helper/RemoteKey.swift
+++ QRArtGenerator/Helper/RemoteKey.swift
@@ -12,4 +12,15 @@ extension RemoteKey {
     static let subscriptionList = RemoteKey(rawValue: "subscription_list")
     static let subGenerateQr = RemoteKey(rawValue: "sub_generate_qr")
     static let subReGenerateQr = RemoteKey(rawValue: "sub_regenerate_qr")
+    
+    static let native_home = RemoteKey(rawValue: "native_home")
+    static let inter_template = RemoteKey(rawValue: "inter_template")
+    static let banner_result = RemoteKey(rawValue: "banner_result")
+    static let inter_generator = RemoteKey(rawValue: "inter_generator")
+    static let native_resultback = RemoteKey(rawValue: "native_resultback")
+    static let reward_regen = RemoteKey(rawValue: "reward_regen")
+    static let inter_createmore = RemoteKey(rawValue: "inter_createmore")
+    static let inter_scanopenlink = RemoteKey(rawValue: "inter_scanopenlink")
+    static let inter_openmail = RemoteKey(rawValue: "inter_openmail")
+    static let banner_scan = RemoteKey(rawValue: "banner_scan")
 }
